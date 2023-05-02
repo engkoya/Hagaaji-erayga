746998dc4c9d880cffef2e1735fecc844fa4ee42
engkoya <129959268+engkoya@users.noreply.github.com>
2023-05-02 13:20:32 +0300

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "Afrika",
+        hint: "Mesha ay kunol yihiin dad leh midab madow"
+    },
+    {
+        word: "Dawaco",
+        hint: "Xaywaanka ugu caqliga badan duur joogta"
+    },
+    {
+        word: "AsadHagio",
+        hint: "Waa youTuber heysto in kabadan 1 Million oo dad ah"
+    },
+    {
+        word: "Yaxaas",
+        hint: "Xaywaan an wax carab ah ku oolin"
+    },
+    {
+        word: "Muqdisho",
+        hint: "Caasimada somaliya"
+    },
+    {
+        word: "Ramadaan",
+        hint: "Bishii uu quraanka soodagay"
+    },
+    {
+        word: "Burco",
+        hint: "Gobolka loyaqano gobolka ragga"
+    },
+    {
+        word: "Hooyo",
+        hint: "Qofka sida dhabta ah kujecel"
+    },
+    {
+        word: "TikTok",
+        hint: "Aalada ugu sameynta badan social media"
+    },
+    {
+        word: "Netflix",
+        hint: "Goobta filimada ugu badan lagadawado"
+    },
+    {
+        word: "Canjeelo",
+        hint: "Qureecda ugu badan ee subixi lagacuno somaliya"
+    },
+    {
+        word: "Dirisle",
+        hint: "YouTuber heysta inkabadan 1Million"
+    },
+    {
+        word: "group",
+        hint: "Tiro dad ah oo hal meel kujiro"
+    },
+    {
+        word: "ukun",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+]
